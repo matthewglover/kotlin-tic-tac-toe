70accf39d54ca770089f201e4847d6ba47174737
Matthew Glover <matthewglover@Matthews-MacBook-Pro.local>
2021-12-23 12:30:04 +0000

    Add Line tests

--- src/test/kotlin/digital/fortisgreen/kotlin/tictactoe/LineTest.kt
+++ src/test/kotlin/digital/fortisgreen/kotlin/tictactoe/LineTest.kt
@@ -2,6 +2,9 @@ package digital.fortisgreen.kotlin.tictactoe
 
 import digital.fortisgreen.kotlin.tictactoe.exceptions.LineCreationException
 import org.junit.jupiter.api.Assertions.assertDoesNotThrow
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Assertions.assertFalse
+import org.junit.jupiter.api.Assertions.assertTrue
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertThrows
 
@@ -19,5 +22,35 @@ internal class LineTest {
         assertThrows<LineCreationException> {
             Line(listOf(null, PlayerMark.X))
         }
+    }
+
+    @Test
+    internal fun `reports winner when X wins`() {
+        val line = Line(listOf(PlayerMark.X, PlayerMark.X, PlayerMark.X))
+        assertTrue(line.isWinner())
+    }
+
+    @Test
+    internal fun `reports winner when O wins`() {
+        val line = Line(listOf(PlayerMark.O, PlayerMark.O, PlayerMark.O))
+        assertTrue(line.isWinner())
+    }
+
+    @Test
+    internal fun `does not report winner when no winner`() {
+        val line = Line(listOf(null, null, null))
+        assertFalse(line.isWinner())
+    }
+
+    @Test
+    internal fun `reports X when it is the winner`() {
+        val line = Line(listOf(PlayerMark.X, PlayerMark.X, PlayerMark.X))
+        assertEquals(PlayerMark.X, line.playerMark())
+    }
+
+    @Test
+    internal fun `reports O when it is the winner`() {
+        val line = Line(listOf(PlayerMark.O, PlayerMark.O, PlayerMark.O))
+        assertEquals(PlayerMark.O, line.playerMark())
     }
 }
